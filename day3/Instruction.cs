dd0dd3d9213fda6eaaaa4d0ae0680ba1afa366ce
AlgoRythm-Dylan <dperunko@comcast.net>
2024-12-04 16:04:39 -0500

    day 3 part 2

---
+++ day3/Instruction.cs
@@ -0,0 +1,20 @@
+﻿namespace day3
+{
+    public enum InstructionType
+    {
+        Mul,
+        Do,
+        Dont
+    }
+    internal class Instruction
+    {
+        public InstructionType Type;
+        public uint Index, Length;
+        public Instruction(InstructionType type, uint index, uint length)
+        {
+            Type = type;
+            Index = index;
+            Length = length;
+        }
+    }
+}
--- day3/Program.cs
+++ day3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace day3
 {
@@ -7,6 +8,7 @@ namespace day3
         static void Main(string[] args)
         {
             PartOne();
+            PartTwo();
         }
         static void PartOne()
         {
@@ -22,6 +24,55 @@ namespace day3
                 total += (uint)lhs * (uint)rhs;
             }
             Console.WriteLine($"Total: {total}");
+        }
+        static void PartTwo()
+        {
+            var mulRegex = new Regex("mul\\([0-9]+,[0-9]+\\)");
+            var doRegex = new Regex("do\\(\\)");
+            var dontRegex = new Regex("don't\\(\\)");
+            List<Instruction> instructions = new();
+            var text = File.ReadAllText("input.txt");
+            foreach (var match in mulRegex.EnumerateMatches(text))
+            {
+                instructions.Add(new Instruction(InstructionType.Mul,
+                                 (uint)match.Index,
+                                 (uint)match.Length));
+            }
+            foreach (var match in doRegex.EnumerateMatches(text))
+            {
+                instructions.Add(new Instruction(InstructionType.Do,
+                                 (uint)match.Index,
+                                 (uint)match.Length));
+            }
+            foreach (var match in dontRegex.EnumerateMatches(text))
+            {
+                instructions.Add(new Instruction(InstructionType.Dont,
+                                 (uint)match.Index,
+                                 (uint)match.Length));
+            }
+            var orderedInstructions = instructions.OrderBy(item => item.Index).ToList();
+            bool countingEnabled = true;
+            uint total = 0;
+            foreach(var instruction in orderedInstructions)
+            {
+                if (instruction.Type == InstructionType.Do)
+                {
+                    countingEnabled = true;
+                }
+                else if (instruction.Type == InstructionType.Dont)
+                {
+                    countingEnabled = false;
+                }
+                else if(instruction.Type == InstructionType.Mul && countingEnabled)
+                {
+                    var math = text.Substring((int)instruction.Index, (int)instruction.Length);
+                    var split = math.Split(',');
+                    var lhs = int.Parse(split[0].Replace("mul(", ""));
+                    var rhs = int.Parse(split[1].Replace(")", ""));
+                    total += (uint)lhs * (uint)rhs;
+                }
+            }
+            Console.WriteLine($"Total of muls with enable/disable: {total}");
         }
     }
 }
