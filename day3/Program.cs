b5c05b3be37d684f97bac48d0c30075b2a1644d9
AlgoRythm-Dylan <dperunko@comcast.net>
2024-12-04 15:49:02 -0500

    day 3 part 1

---
+++ day3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+        }
+        static void PartOne()
+        {
+            var regex = new Regex("mul\\([0-9]+,[0-9]+\\)");
+            var text = File.ReadAllText("input.txt");
+            uint total = 0;
+            foreach (var match in regex.EnumerateMatches(text))
+            {
+                var instruction = text.Substring(match.Index, match.Length);
+                var split = instruction.Split(',');
+                var lhs = int.Parse(split[0].Replace("mul(", ""));
+                var rhs = int.Parse(split[1].Replace(")", ""));
+                total += (uint)lhs * (uint)rhs;
+            }
+            Console.WriteLine($"Total: {total}");
+        }
+    }
+}
