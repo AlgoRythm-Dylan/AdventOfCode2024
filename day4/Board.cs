f42de6909d97ddfeadb83e6f65455c975b4c74f6
AlgoRythm-Dylan <dperunko@comcast.net>
2024-12-10 01:16:57 -0500

    im STUCK

---
+++ day4/Board.cs
@@ -0,0 +1,36 @@
+﻿namespace day4
+{
+    internal class Board
+    {
+        public string Cells;
+        public int Width { get; private set; } = 0;
+        public int Height { get; private set; } = 0;
+        public Board(string cells)
+        {
+            Cells = cells;
+            ParseDimensions();
+        }
+        private void ParseDimensions()
+        {
+            Width = 0;
+            Height = 0;
+
+            foreach (var character in Cells)
+            {
+                if (character == '\n')
+                {
+                    Height++;
+                }
+                else if (Height == 0)
+                {
+                    // Count characters in the first row to determine Width
+                    Width++;
+                }
+            }
+        }
+        public char At(int x, int y)
+        {
+            return Cells[x + ((Width + 1) * y)];
+        }
+    }
+}
---
+++ day4/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+        }
+        static void PartOne()
+        {
+            var board = new Board(File.ReadAllText("input.txt"));
+            //var count = Regex.Count(board.Cells, "XMAS");
+            Console.WriteLine($"Matches found: {Search(board, "XMAS") + Search(board, "SAMX")}");
+        }
+        static int Search(Board board, string term)
+        {
+            return SearchHorizontally(board, term) +
+                   SearchVertically(board, term) +
+                   SearchDiagonallyRight(board, term) +
+                   SearchDiagonallyLeft(board, term);
+        }
+        static int SearchHorizontally(Board board, string term)
+        {
+            int matches = 0;
+            for(int y = 0; y < board.Height; y++)
+            {
+                for(int x = 0; x < board.Width - (term.Length - 1); x++)
+                {
+                    int matchIndex = 0;
+                    for(int i = 0; i < term.Length; i++)
+                    {
+                        if (board.At(x + i, y) == term[i])
+                        {
+                            matchIndex++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if(matchIndex == term.Length)
+                    {
+                        matches++;
+                    }
+                }
+            }
+            return matches;
+        }
+        static int SearchVertically(Board board, string term)
+        {
+            int matches = 0;
+            for (int x = 0; x < board.Width; x++)
+            {
+                for (int y = 0; y < board.Height - (term.Length - 1); y++)
+                {
+                    int matchIndex = 0;
+                    for (int i = 0; i < term.Length; i++)
+                    {
+                        if (board.At(x + i, y) == term[i])
+                        {
+                            matchIndex++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if (matchIndex == term.Length)
+                    {
+                        matches++;
+                    }
+                }
+            }
+            return matches;
+        }
+        static int SearchDiagonallyRight(Board board, string term)
+        {
+            int matches = 0;
+            for (int x = 0; x < board.Width - (term.Length - 1); x++)
+            {
+                for (int y = 0; y < board.Height - (term.Length - 1); y++)
+                {
+                    int matchIndex = 0;
+                    for(int i = 0; i < term.Length; i++)
+                    {
+                        if (board.At(x + i, y + i) == term[i])
+                        {
+                            matchIndex++;
+                        }
+                    }
+                    if(matchIndex == term.Length)
+                    {
+                        matches++;
+                    }
+                }
+            }
+            return matches;
+        }
+        static int SearchDiagonallyLeft(Board board, string term)
+        {
+            int matches = 0;
+            for (int x = term.Length; x < board.Width; x++)
+            {
+                for (int y = 0; y < board.Height - (term.Length - 1); y++)
+                {
+                    int matchIndex = 0;
+                    for (int i = 0; i < term.Length; i++)
+                    {
+                        if (board.At(x - i, y + i) == term[i])
+                        {
+                            matchIndex++;
+                        }
+                    }
+                    if (matchIndex == term.Length)
+                    {
+                        matches++;
+                    }
+                }
+            }
+            return matches;
+        }
+    }
+}
